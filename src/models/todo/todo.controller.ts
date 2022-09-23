3af4c881aca7f016cb5766682c5892895c292265
erickkartiadi <erickcartiady@gmail.com>
2022-09-23 12:23:56 +0700

    :sparkles: include todos data when CRUD

--- src/models/todo/todo.controller.ts
+++ src/models/todo/todo.controller.ts
@@ -15,13 +15,21 @@ import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import RequestWithUser from 'types/request-with-user.interface';
 
 import { CreateTodoDto } from './dto/create-todo.dto';
+import { UpdateTodoDto } from './dto/update-todo.dto';
 import { TodoService } from './todo.service';
+
+// TODO add GetUser decorator
 
 @Controller('todo')
 @UseGuards(JwtAuthGuard)
 @UsePipes(new ValidationPipe())
 export class TodoController {
   constructor(private readonly todoService: TodoService) {}
+
+  @Get()
+  findAll(@Req() req: RequestWithUser) {
+    return this.todoService.findAll(req.user.id);
+  }
 
   @Post()
   create(@Body() createTodoDto: CreateTodoDto, @Req() req: RequestWithUser) {
@@ -37,22 +45,8 @@ export class TodoController {
   update(
     @Req() req: RequestWithUser,
     @Param('id') id: string,
-    @Body() updateTodoDto: CreateTodoDto
+    @Body() updateTodoDto: UpdateTodoDto
   ) {
     return this.todoService.update(req.user.id, id, updateTodoDto);
-  }
-
-  @Patch('toggle/:id')
-  toggle(
-    @Req() req: RequestWithUser,
-    @Param('id') id: string,
-    @Body('isCompleted') isCompleted: boolean
-  ) {
-    return this.todoService.toggleCompleted(req.user.id, id, isCompleted);
-  }
-
-  @Get()
-  findAll(@Req() req: RequestWithUser) {
-    return this.todoService.findAll(req.user.id);
   }
 }
--- src/models/todo/todo.service.ts
+++ src/models/todo/todo.service.ts
@@ -3,23 +3,11 @@ import { Todo, User } from '@prisma/client';
 import { PrismaService } from 'src/providers/prisma/prisma.service';
 
 import { CreateTodoDto } from './dto/create-todo.dto';
+import { UpdateTodoDto } from './dto/update-todo.dto';
 
 @Injectable()
 export class TodoService {
   constructor(private prisma: PrismaService) {}
-
-  async create(userId: string, createTodoDto: CreateTodoDto): Promise<User> {
-    return this.prisma.user.update({
-      where: {
-        id: userId
-      },
-      data: {
-        todos: {
-          create: createTodoDto
-        }
-      }
-    });
-  }
 
   async findAll(userId: string): Promise<
     | (User & {
@@ -37,6 +25,19 @@ export class TodoService {
     });
   }
 
+  async create(userId: string, createTodoDto: CreateTodoDto): Promise<Todo> {
+    return this.prisma.todo.create({
+      data: {
+        userId,
+        completed: createTodoDto.completed,
+        todo: createTodoDto.todo,
+        importanceLevel: createTodoDto.importanceLevel,
+        reminderTime: createTodoDto.reminderTime,
+        note: createTodoDto.note
+      }
+    });
+  }
+
   async delete(userId: string, id: string) {
     return this.prisma.user.update({
       where: {
@@ -46,11 +47,14 @@ export class TodoService {
         todos: {
           delete: { id: id }
         }
+      },
+      include: {
+        todos: true
       }
     });
   }
 
-  async update(userId: string, id: string, updateTodoDto: CreateTodoDto) {
+  async update(userId: string, id: string, updateTodoDto: UpdateTodoDto) {
     return this.prisma.user.update({
       where: {
         id: userId
@@ -64,26 +68,9 @@ export class TodoService {
             data: updateTodoDto
           }
         }
-      }
-    });
-  }
-
-  async toggleCompleted(userId: string, id: string, isCompleted: boolean) {
-    return this.prisma.user.update({
-      where: {
-        id: userId
       },
-      data: {
-        todos: {
-          update: {
-            where: {
-              id: id
-            },
-            data: {
-              completed: isCompleted
-            }
-          }
-        }
+      include: {
+        todos: true
       }
     });
   }
